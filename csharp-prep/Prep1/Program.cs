0b811512b6daa52a46d9ed8834398a9c25f0ae0e
Nathen Cheney <168137218+ncheney5@users.noreply.github.com>
2025-04-22 09:24:33 -0600

    wrote the two programs for week one

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("what is your first name?" );
+        string name = Console.ReadLine();
+        Console.Write("what is your last name?" );
+        String last = Console.ReadLine();
+        Console.WriteLine($"your name is {last}, {name} {last}");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("what is the grade percentage? ");
+        string percentage = Console.ReadLine();
+        int percent= int.Parse(percentage);
+        string letter;
+        if (percent>90)
+        {  
+            letter ="A";
+        }
+        else if (percent>80)
+        {
+            letter="B";
+        }
+        else if (percent>70)
+        {
+            letter="C";
+        }
+        else if (percent>60)
+        {
+            letter="D";
+        }
+        else
+        {
+            letter="F";
+        }
+        if (percent>70)
+        {
+            Console.WriteLine($"You got a {letter}");
+            Console.WriteLine("You passed the class! Congradualtions");
+        }
+        else
+        {
+            Console.WriteLine($"You got a {letter}");
+            Console.WriteLine("you did not pass");
+        }
     }
 }
